c072eff0eaab8998d4a0d6464705258b4e25281c
Tasin Ahmmed <122511332+BANTUBD@users.noreply.github.com>
2023-12-21 08:56:42 +0600

    V

--- TMC.py
+++ TMC.py
@@ -1,4 +1,4 @@
 import os, platform, time, sys
 os.system('git pull')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests futures==2 > /dev/null')
-import FS_enc
+import F_enc
